704934e03114640e53cab112cd718907283e6397
Leacius <58459135+Leacius@users.noreply.github.com>
2024-05-10 23:44:50 +0800

    Update api.py

--- api.py
+++ api.py
@@ -2,14 +2,9 @@ import requests
 import json
 import pandas as pd
 
-# response = requests.get("http://127.0.0.1:8080/")
-# resultText = response.text
-
-# print(resultText)
-
-for i in range(10):
-    res = requests.post('http://127.0.0.1:8080/Generate', json={"Type": "int"})
-    if res.ok:
-        print(res.json())
-
-# 呼叫 10 個變數
+if __name__ == "__main__":
+    # 呼叫 10 個變數
+    for i in range(10):
+        res = requests.post('http://127.0.0.1:8080/Generate', json={"Type": "int"})
+        if res.ok:
+            print(res.json())
