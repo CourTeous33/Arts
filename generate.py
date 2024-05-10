18f61717db5bd7bf7de1c5a53f24514dbc19c4ba
Leacius <58459135+Leacius@users.noreply.github.com>
2024-05-10 23:43:57 +0800

    Delete generate.py

--- generate.py
+++
@@ -1,9 +0,0 @@
-import time
-import numpy as np
-
-if __name__ == "__main__":
-    while True:
-        # Generate a random number
-        with open("test.json", "w") as f:
-            f.write(str(np.random.randint(1, 100)))
-        time.sleep(100)
